be1fd7a8b58e36108f822fe611de52eb7595e666
Keith Mutugi <keithmutugi64@gmail.com>
2026-04-01 16:34:02 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 // Write your code here!
+const userInfo = {
+  username: "flatUser123",
+  description: "Hi, welcome to my webpage! Just a new user of this website!",
+  theme: "red"
+};
 
+function displayUser(userInfo) {
+  // Select header
+  const header = document.querySelector("#main");
+
+  // Update header text and style
+  header.textContent = userInfo.username + "'s Personal Webpage";
+  header.className = userInfo.theme;
+
+  // Create description element
+  const description = document.createElement("p");
+
+  // Select body
+  const body = document.querySelector("body");
+
+  // Add content and style to description
+  description.textContent = userInfo.description;
+  description.className = userInfo.theme;
+
+  // Append description to the page
+  body.append(description);
+}
+
+// Call the function
+displayUser(userInfo);
